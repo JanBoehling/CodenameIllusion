dc627c636d5a24d612f2523a5fe670368ae1fd9c
Jordan-Lee <jordanlgh@outlook.com>
2024-05-13 16:48:25 +0200

    Deleted Platforms

--- Platforms/Mac/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudio/PCH.FMODStudio.h
+++
@@ -1,3 +0,0 @@
-// PCH for C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h
-#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudio/Definitions.h"
-#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Source/FMODStudio/Private/FMODStudioPrivatePCH.h"
--- Platforms/Mac/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/PCH.FMODStudioEditor.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.39.33523
-#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h"
