6d6145d227121d0fcf50d246cbcaf53309c9331c
JanBoehling <janrasmus03.boehling@t-online.de>
2024-05-13 16:32:41 +0200

    Added platforms folder

---
+++ Platforms/Mac/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudio/PCH.FMODStudio.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.39.33523
+#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudio/PCH.FMODStudio.h"
---
+++ Platforms/Mac/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h
@@ -0,0 +1,3 @@
+// PCH for C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioEditorPrivatePCH.h
+#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Intermediate/Build/Win64/x64/UnrealEditor/Development/FMODStudioEditor/Definitions.h"
+#include "C:/_5FSC0XD102/CodenameIllusion/Plugins/FMODStudio/Source/FMODStudioEditor/Private/FMODStudioEditorPrivatePCH.h"
