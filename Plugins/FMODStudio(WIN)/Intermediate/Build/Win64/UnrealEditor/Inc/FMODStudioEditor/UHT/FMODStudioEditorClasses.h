2447429343e52940f87d00def046c57dd258d718
Jordan-Lee <jordanlgh@outlook.com>
2024-07-08 16:35:09 +0200

    Fix FMOD Plugin (pls kill me)

---
+++ Plugins/FMODStudio(WIN)/Intermediate/Build/Win64/UnrealEditor/Inc/FMODStudioEditor/UHT/FMODStudioEditorClasses.h
@@ -0,0 +1,12 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#pragma once
+
+
+#include "C:/BuildAgent1/work/570ad728b1678096/build/staging/FMODStudio/HostProject/Plugins/FMODStudio/Source/FMODStudioEditor/Classes/FMODAmbientSoundActorFactory.h"
+#include "C:/BuildAgent1/work/570ad728b1678096/build/staging/FMODStudio/HostProject/Plugins/FMODStudio/Source/FMODStudioEditor/Classes/FMODGenerateAssetsCommandlet.h"
+
